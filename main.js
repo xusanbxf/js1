e2215869bef00bce2cf4c0de7ed4bc15b249a02b
xusan <xusan007>
2022-10-22 11:10:00 +0500

    js1

---
+++ main.js
@@ -0,0 +1,17 @@
+let yosh = +prompt("Yoshingizni kiriting") ;
+
+if (0 < yosh && yosh <= 18) {
+  alert('o`qshingiz kerak')
+}else if(18 < yosh && yosh <= 50){
+  alert('siz ishlashingiz kerak')
+}else if (50 < yosh && yosh <= 59) {
+  alert('yaqinda pensiyaga ciqasiz')
+}else if (59 < yosh && yosh <= 150) {
+  alert('pensiyonerga oxshaysiz, hali trik bo`lsangiz')
+}else(
+  alert('Nimadur notog`ri ketib qoldi')
+) 
+
+
+
+    
